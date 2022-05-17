7eee5a4f129a810547a57316e0cde5cc75d95821
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2022-05-17 08:28:06 -0500

    psuedo_binary_search

---
+++ algorithms/searching/binary_search/psuedo_binary_search.py
@@ -0,0 +1,53 @@
+# Import random module
+
+
+# Define function with two parameters data and target
+
+    # Declare low_pointer variable
+
+    # Declare high_pointer variable
+
+    # While loop with low_pointer less than or equal to high pointer
+
+        # Declare mid_point variable bu add low_pointer plus highpoint and then divide by two
+
+        # if mid_point is greater than the target value
+
+            # Return midpoint
+
+        # elif data midpoint is greater than target value
+
+            # Declare low_point by mid_point + 1
+
+
+            # else, declare highpoint var with midpoint - 1
+
+    # return -1
+
+
+# declare variable n with value 10
+
+
+# declare max value with 100
+
+
+# declare data var using random module with 1 and max value. Run for loop against n value
+
+
+# Sort the numbers in data var
+
+# Print sorted data
+
+# Let user to enter the target value
+
+
+# Declare target position using binary search function with the two parameters data and target
+
+# if target position is equal to -1
+
+    # Print out a message your taget value is not in the list
+
+# else
+
+    # Print out a message "your target value is at index" , call the target position variable
+
