3f4a9e8d2d2f5d652c1a32c1036bbcce4089d681
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2022-05-30 12:09:20 -0500

    Conditionals Update

---
+++ conditionals/a_if_then_else.py
@@ -0,0 +1,13 @@
+furry = True
+small = False
+
+if furry:
+    if small:
+        print("it's a cat")
+    else:
+        print("it's a bear")
+else:
+    if small:
+        print("it's a skunk")
+    else:
+        print("it's a human")
