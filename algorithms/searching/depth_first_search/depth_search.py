845e926b0139b462e261192d6cf33d6699929af7
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2023-06-08 20:37:04 -0500

    depthfirst - Thu Jun  8 20:37:04 CDT 2023

---
+++ algorithms/searching/depth_first_search/depth_search.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3
+
+graph = {
+    '5' : ['3','7'],
+    '3' : ['2', '4'],
+    '7' : ['8'],
+    '2' : [],
+    '4' : ['8'],
+    '8' : []
+}
+
+visited = set()
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+print("Depth-First Search demo")
+dfs(visited, graph, '5')
+
