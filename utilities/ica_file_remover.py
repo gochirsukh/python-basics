312873a0faea9fde2e19a9e9298dbf8fbbea25f3
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2024-07-24 20:53:53 -0700

    python script

---
+++ utilities/ica_file_remover.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# Citrix client creates .ica files in ~/Download directory and they don't go anywhere. 
+# This code removes .ica files in a given directory effortlessly 
+# This is just a quick script i wrote under 20 min and it has some room for improvement
+
+import os
+
+def list_ica(directory):
+    files = [f for f in os.listdir(directory) if f.endswith('.ica')]
+
+    if files: 
+        print("ICA files in directory")
+        for file in files: 
+            print(file)            
+    else:
+        print("No ICA files found")
+
+def remove_ica(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith('.ica'):
+            file_path = os.path.join(directory, filename)
+            try: 
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+            except:
+                print("Something is wrong")
+
+list_ica('/tmp/dumbster')
+
+response = input("Would you like to remove them, y/n: ")
+
+if response.lower() == 'y':
+    print("Removing the files")
+    remove_ica('/tmp/dumbster')
+else:
+    print("Answer was no or invalid entry, exit")
+    
