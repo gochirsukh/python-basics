320c1f597f195302f315aeaaf05fb8bca08b7353
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2024-12-25 11:27:43 -0800

    update

--- functions/rectangle.py
+++ functions/rectangle.py
@@ -1,19 +1,16 @@
+# Simple example for finding aread of rectable 
 
 import sys 
 
 
-
-
-
-def find_ractanle(length,  width):
+def find_rectangle (length,  width):
     
     if length <= 0 or width <=0:
         print("Length have to be positive number")
         sys.exit(1) 
 
-    rectancle = length * width
-    print("The size of rectanle with", "length of", length, "and width of", width, "is", rectancle)
-
+    rectangle = length * width
+    print("The size of rectangle with", "length of", length, "and width of", width, "is", rectangle)
 
 
 length=int(input("Enter length: "))
@@ -22,4 +19,4 @@ print("You have", length, "for length")
 width=int(input("Enter width: "))
 print("You have", width, "for width")
 
-find_ractanle(length, width)
+find_rectangle(length, width)
